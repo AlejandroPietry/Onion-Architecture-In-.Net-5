ed917e34540a1126a1bce2bc39a2ab49d8c614b3
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-03-21 19:50:11 -0300

    Finalizando interface e implementacao

--- RepositoryLayer/RepositoryPattern/Repository.cs
+++ RepositoryLayer/RepositoryPattern/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,64 @@ using System.Threading.Tasks;
 
 namespace RepositoryLayer.RepositoryPattern
 {
-    public class Repository<T> : IRepository<T> where T: BaseEntity
+    public class Repository<T> : IRepository<T> where T : BaseEntity
     {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> entities;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            entities = context.Set<T>();
+        }
+        public void Delete(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException("entity");
+
+            entities.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public T Get(int Id)
+        {
+            return entities.SingleOrDefault(x => x.Id == Id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException("entity");
+            entities.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException("entity");
+
+            entities.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException("entity");
+
+            entities.Update(entity);
+            _context.SaveChanges();
+        }
     }
 }
