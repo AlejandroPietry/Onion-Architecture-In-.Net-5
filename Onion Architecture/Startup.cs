1e9602bf119a6e6fe8642081db5fc34051f98307
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-03-21 21:58:17 -0300

    retirando using desnecessarios

--- Onion Architecture/Startup.cs
+++ Onion Architecture/Startup.cs
@@ -1,20 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using RepositoryLayer;
 using RepositoryLayer.RepositoryPattern;
 using ServicesLayer.CustomerService;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Onion_Architecture
 {
