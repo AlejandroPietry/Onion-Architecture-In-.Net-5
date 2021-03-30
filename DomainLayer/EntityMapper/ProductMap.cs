772b30392abaa91e918295bd5ee91a723f3b1125
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-03-30 07:58:43 -0300

    craindo model do produto e map

---
+++ DomainLayer/EntityMapper/ProductMap.cs
@@ -0,0 +1,14 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DomainLayer.EntityMapper
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DomainLayer/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace DomainLayer.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
