5d3e96eb458a355d43f7084f0771de9ad2b9a363
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-04-13 21:43:31 -0300

    Rodando code cleanup

--- Onion Architecture/Controllers/ProductController.cs
+++ Onion Architecture/Controllers/ProductController.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServicesLayer.ProductService;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Onion_Architecture.Controllers
 {
     public class ProductController : ControllerBase
     {
         private IProductService _productService;
-        
+
         public ProductController(IProductService productService)
         {
             _productService = productService;
@@ -18,7 +14,7 @@ namespace Onion_Architecture.Controllers
 
         public IActionResult GetProduct(int id)
         {
-            return  Ok(_productService.GetProduct(id));
+            return Ok(_productService.GetProduct(id));
         }
 
         public IActionResult GetAllProducts()
